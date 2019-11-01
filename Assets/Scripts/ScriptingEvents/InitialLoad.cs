479a559fb2b498b0504c2b4ed68a63045c3386ce
AJ Abotomey <killerbyte_93@hotmail.com>
2019-11-01 17:23:52 +1000

    More scripting of the first level

--- Assets/Scripts/ScriptingEvents/InitialLoad.cs
+++ Assets/Scripts/ScriptingEvents/InitialLoad.cs
@@ -6,11 +6,25 @@ public class InitialLoad : MonoBehaviour
 {
     [SerializeField] private Notification firstNotification;
     [SerializeField] private Notification movement;
+    [SerializeField] private Notification rotation;
+    [SerializeField] private Notification shooting;
+    [SerializeField] private Notification window;
 
     // Start is called before the first frame update
     void Start()
     {
         //GameUIController.Instance.PushNotification(firstNotification);
+        StartCoroutine(PushNotifications());
+    }
+
+    IEnumerator PushNotifications()
+    {
         GameUIController.Instance.PushNotification(movement);
+        yield return new WaitForSeconds(3);
+        GameUIController.Instance.PushNotification(rotation);
+        yield return new WaitForSeconds(3);
+        GameUIController.Instance.PushNotification(shooting);
+
+        yield return null;
     }
 }
