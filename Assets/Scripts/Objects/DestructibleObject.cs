8d84885c05209907c70487825da8fd3abc89feb6
AJ Abotomey <killerbyte_93@hotmail.com>
2019-11-01 11:03:01 +1000

    Forgot to add the Destructible Object class

---
+++ Assets/Scripts/Objects/DestructibleObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleObject : MonoBehaviour
+{
+    [SerializeField] private int health;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health <= 0) {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+    }
+}
