46079d063caca4d7fd7c594614b9135fc175fadf
AJ Abotomey <killerbyte_93@hotmail.com>
2019-11-05 23:24:08 +1000

    Started developing the A* Pathfinding system. Should utilise the tile system along with Multithreading

---
+++ Assets/Scripts/AI/AStar/AStarCompiler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AStarCompiler : MonoBehaviour
+{
+    [SerializeField] private LayerMask[] obstacleMasks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
