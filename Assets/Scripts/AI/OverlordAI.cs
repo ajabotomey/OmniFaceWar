e781f84863b501ed9b573fc75096aeec3964c058
AJ Abotomey <killerbyte_93@hotmail.com>
2020-01-09 21:11:49 +1000

    Forgot to add the Overlord AI files

---
+++ Assets/Scripts/AI/OverlordAI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class OverlordAI : MonoBehaviour
+{
+    [SerializeField] private List<AIAgent> agents = new List<AIAgent>();
+
+    public List<AIAgent> GetAgents() { 
+        return agents; 
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        AIAgent agent = other.GetComponent<AIAgent>();
+        if (!agents.Contains(agent)) { 
+            agents.Add(agent);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        agents.Remove(other.GetComponent<AIAgent>());
+    }
+
+    public void AlertAllAgents()
+    {
+        foreach (AIAgent agent in agents) {
+            agent.SetAgentToFullAlert();
+        }
+    }
+}
