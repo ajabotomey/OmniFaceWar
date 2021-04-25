f24c430d3bc52e958d5f129d6b89ff9558eb483d
AJ Abotomey <killerbyte_93@hotmail.com>
2021-04-25 12:42:11 +1000

    Added unit tests + Test Runner workflow
    Now have Unit Tests for testing the Input System.

--- Assets/Tests/Input Tests/InputTesting.cs
+++ Assets/Tests/Input Tests/InputTesting.cs
@@ -4,13 +4,63 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
+using UnityEngine.InputSystem.Utilities;
+using UnityEngine.SceneManagement;
 
-[TestFixture]
-public class InputTesting
+public class InputTesting : InputTestFixture
 {
+    //private Keyboard keyboard;
+    //private Mouse mouse;
+    //private Gamepad gamepad;
+
+    //[OneTimeSetUp]
+    //public void InitializeTestEnvironment()
+    //{
+    //    // Load our scene
+    //    //SceneManager.LoadScene("TutorialLevel");
+
+    //    // Attach our devices
+    //    keyboard = InputSystem.AddDevice<Keyboard>();
+    //    mouse = InputSystem.AddDevice<Mouse>();
+    //    gamepad = InputSystem.AddDevice<Gamepad>();
+
+    //    // Wait for enough time to load the scene
+    //    //yield return new WaitForSeconds(10);
+    //}
+
     [Test]
-    public void InputTesting_CanUseKeyboardAndMouse()
+    public void InputTesting_KeyboardMovement()
     {
         var keyboard = InputSystem.AddDevice<Keyboard>();
+
+        var movement = new InputAction("Movement", InputActionType.Value);
+        movement.AddCompositeBinding("2DVector")
+            .With("Up", "<Keyboard>/w")
+            .With("Down", "<Keyboard/s")
+            .With("Left", "<Keyboard/a")
+            .With("Right", "<Keyboard/d");
+
+        movement.Enable();
+
+        // Test W Key
+        Press(keyboard.wKey);
+        Assert.That(movement.ReadValue<Vector2>(), Is.EqualTo(new Vector2(0, 1)));
+        Release(keyboard.wKey);
+
+        // Test S Key
+        Press(keyboard.sKey);
+        Assert.That(movement.ReadValue<Vector2>(), Is.EqualTo(new Vector2(0, -1)));
+        Release(keyboard.sKey);
+
+        // Test A Key
+        Press(keyboard.aKey);
+        Assert.That(movement.ReadValue<Vector2>(), Is.EqualTo(new Vector2(-1, 0)));
+        Release(keyboard.aKey);
+
+        // Test D Key
+        Press(keyboard.dKey);
+        Assert.That(movement.ReadValue<Vector2>(), Is.EqualTo(new Vector2(1, 0)));
+        Release(keyboard.dKey);
     }
 }
